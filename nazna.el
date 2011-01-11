e385eb057e57b6daf395085bb6aa6a7293b99200
baron <richstyles@gmail.com>
2011-01-11 16:46:15 +0900

    finished first iteration

--- nazna.el
+++ nazna.el
@@ -9,15 +9,16 @@
 ;; Last-Updated:
 ;;           By:
 ;;     Update #: 0
-;; URL:
-;; Keywords:
+;; URL: https://github.com/baron/nazna.el
+;; Keywords: arithmetic, math
 ;; Compatibility:
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;; Commentary:
 ;; mental arithmetic for emacs
-;;
+;; nazna is "anzan", that is
+;; mental arithmetic in Japanese spelled backwards
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -45,11 +46,20 @@
 ;;
 ;;; Code:
 
+(require 'cl)
+
 (defcustom nazna-operators '("*" "/" "+" "-")
   "the operators for use in generating problems")
 
 (defcustom nazna-difficulty 1
   "difficulty level of problems, corresponds to the number of maximum digits in random number generator")
+
+(defcustom nazna-level-step 2
+  "this variable sets how fast the difficulty is incremented")
+
+(defvar nazna-wrong-answers 0)
+
+(defvar nazna-correct-answers 0)
 
 (defun nazna-random-number ()
   "generates random number for problems"
@@ -57,18 +67,64 @@
 
 (defun nazna-operator ()
   "selects random operator"
-  (nth (- (length nazna-operators) 1) nazna-operators))
+  (nth (random (- (length nazna-operators) 1)) nazna-operators))
 
-(defun nazna-make-problem ()
-  "creates arithmetic problem"
+(defun nazna-solution  (x y op)
+  (cond
+   ((string= op "*") (* x y))
+   ((string= op "-") (- x y))
+   ((string= op "+") (+ x y))
+   ((string= op "/")
+    (if (= y 0)
+        0
+      (/ (float x) (float y))))))
+
+(defun nazna-correct ()
+  (progn
+    (setq nazna-correct-answers (1+ nazna-correct-answers))
+    (message "good job")))
+
+(defun nazna-wrong ()
+  (progn
+    (setq nazna-wrong-answers (1- nazna-wrong-answers))
+    (message (format "wrong: %s %s" question answer))))
+
+(defun nazna-adjust ()
+  (if (> nazna-wrong-answers nazna-level-step)
+      (progn
+        (unless (< nazna-difficulty 2)
+          (setq nazna-difficulty (1- nazna-difficulty)))
+        (setq nazna-wrong-answers 0)))
+  (if (> nazna-correct-answers nazna-level-step)
+      (progn
+        (setq nazna-difficulty (1+ nazna-difficulty))
+        (setq nazna-correct-answers 0))))
+
+(defun nazna-answer-problem (question answer)
   (interactive)
+  (let ((i (read-from-minibuffer question))
+        (sleepint 0.5))
+    (cond
+     ((string= i "q") (message "bye"))
+     ((string= i "s") (nazna))
+     ((if (= (floor answer) (string-to-number i))
+          (nazna-correct)
+        (progn
+          (nazna-wrong)
+          (setq sleepint (1+ sleepint))))
+      (sleep-for sleepint)
+      (nazna)))))
+
+(defun nazna ()
+  "main function creates arithmetic problem"
+  (interactive)
+  (nazna-adjust)
   (let  ((x (nazna-random-number))
          (y (nazna-random-number))
-         (op (nazna-operator))
-         )
-    (message (format "%s %s %s =" x op y))
-    )
-  )
+         (op (nazna-operator)))
+    (let ((ques (format "%s %s %s = " x op y))
+          (ans (nazna-solution x y op)))
+      (nazna-answer-problem ques ans))))
 
 (provide 'nazna)
 
